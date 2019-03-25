5f263970aaa605f80e1cbd3530dbe4a0aeb68765
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-03-25 12:37:54 +0100

    ajout du spam v2

--- java.js
+++ java.js
@@ -1,7 +1,17 @@
 function spam() {
-	alert( "Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
 
-}		
+	if (confirm('Hey le site xxxvidxxx est trop bien. Viens dessus stp please')){
+
+window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "nom_fenetre","options_fenetre")  //action à faire pour la valeur true
+}else{
+  //action à faire pour la valeur false
+  
+} 
+
+}
+
+	
 
 setTimeout (spam, 10000)
 
+ //
