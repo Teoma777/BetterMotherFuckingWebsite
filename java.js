20a356e565f59877602f21e5c4f558a51b2863e4
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-03-30 20:46:40 +0100

    psychedelisme ajouté

--- java.js
+++ java.js
@@ -1,17 +1,64 @@
 function spam() {
+	if (confirm('Hey le site xxxvidxxx est trop bien. Viens dessus stp please'))
+		{window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "nom_fenetre","options_fenetre")}
+else{}  
+				}
 
-	if (confirm('Hey le site xxxvidxxx est trop bien. Viens dessus stp please')){
 
-window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "nom_fenetre","options_fenetre")  //action à faire pour la valeur true
-}else{
-  //action à faire pour la valeur false
-  
-} 
+document.getElementById("clickMe").onclick = doFunction;
+
+
+
+
+
+function doFunction() {
+
+
+
+document.querySelector("body").style.backgroundColor = "magenta";
+
+
+
+document.querySelectorAll("p").forEach(function(e) {e.style.color = "blue"}) ;
+//document.querySelectorAll("p").forEach(function(e) {e.style.style.fontFamily = ' Papyrus' }) ;
+document.querySelectorAll("head").forEach(function(e) {e.style.color = "green"}) ;
+
+
+
+
+
+
+
+
+
+
+//document.querySelectorAll("h1").style.color = "blue";
+//
+//var titre = document.querySelector('h1, h2, h3') ; 
+
+	// document"titre".style.color = "blue";  
+
+
 
 }
 
+
+ 
+
+
+
+//p = document.querySelectorAll("p");
+//
+
+//var bleu = document.querySelectorAll("#bleu").style.color = "blue";
+
+
+
+
+
+
 	
 
-setTimeout (spam, 10000)
+//setTimeout (spam, 10000)
 
  //
